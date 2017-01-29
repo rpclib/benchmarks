9403faa66979d286c3d21e6a46536d6d6fc9227a
sztomi <szelei.t@gmail.com>
2017-01-29 23:29:03 +0100

    Added thrift generation to script

--- scripts/run_codegens.sh
+++ scripts/run_codegens.sh
@@ -8,15 +8,19 @@ BENCHMARKS_ROOT="$SCRIPT_DIR/.."
 pushd $SCRIPT_DIR
 
 # 1. grpc
-echo "Generating grpc code"
+echo "> Generating grpc code"
 protoc --grpc_out=. -I$BENCHMARKS_ROOT/idl --plugin=protoc-gen-grpc=`which grpc_cpp_plugin` $BENCHMARKS_ROOT/idl/grpc_service.proto
-echo "Generating grpc code #2"
+echo "> Generating grpc code #2"
 protoc --cpp_out=. -I$BENCHMARKS_ROOT/idl --plugin=protoc-gen-grpc=`which grpc_cpp_plugin` $BENCHMARKS_ROOT/idl/grpc_service.proto
-mv ./*.h $BENCHMARKS_ROOT/include/grpc
-mv ./*.cc $BENCHMARKS_ROOT/src/grpc
+mv $SCRIPT_DIR/*.h $BENCHMARKS_ROOT/include/grpc
+mv $SCRIPT_DIR/*.cc $BENCHMARKS_ROOT/src/grpc
 
 # 2. thrift
-# TODO
+echo "> Generating thrift code"
+thrift -r --gen cpp $BENCHMARKS_ROOT/idl/thrift_service.thrift
+mv $SCRIPT_DIR/gen-cpp/*.h $BENCHMARKS_ROOT/include/thrift
+mv $SCRIPT_DIR/gen-cpp/*.cpp $BENCHMARKS_ROOT/src/thrift
+rm -r $SCRIPT_DIR/gen-cpp
 
 # 3. cap'n'proto
 # TODO
